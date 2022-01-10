976b85705e16eb34a8c530142ebc85593983ede2
Aryan810 <67587135+Aryan810@users.noreply.github.com>
2022-01-10 15:51:49 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// function randomNumber(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+// function change_image_border_color() {
+//     document.getElementById('image').style.borderColor = `rgb(${randomNumber(9, 255)}, ${randomNumber(20, 200)}, ${randomNumber(25, 255)})`
+// }
+
+// function change_welcome_font_color() {
+//     document.getElementById("text_font").style.color = `rgb(${randomNumber(9, 255)}, ${randomNumber(20, 200)}, ${randomNumber(25, 255)})`
+// }
+
+// function slider() {
+//     if (String(document.getElementById('image').src).endsWith('alpha_a.png')) {
+//         console.log("Yes")
+        
+//     }
+//     else {
+//         document.getElementById('image').src = 'media\\img\\alpha_a.png'
+//     }
+// }
+
+// setInterval(slider, 5000)
+// setInterval(change_welcome_font_color, 1000)
+// setInterval(change_image_border_color, 1000)
+
+
+
